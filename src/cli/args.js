a1fdcda193a8d97d85f22b2f53393138dadfc5a6
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-01 23:56:26 +0400

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,19 @@
+import { argv } from "process";
+
+// для проверки ввести в /node-nodejs-basics "node ./src/cli/args.js --propName value --prop2Name value2"
+
 export const parseArgs = () => {
-    // Write your code here 
+  console.log(
+    argv
+      .slice(2)
+      .reduce((acc, curr, index, array) => {
+        if (curr.startsWith("--")) {
+          return [...acc, `${array[index].slice(2)} is ${array[index + 1]}`];
+        }
+        return acc;
+      }, [])
+      .join(", ")
+  );
 };
+
+parseArgs();
