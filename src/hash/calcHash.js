4851a44ff10cf48ed52714c24e3a550da93b5a56
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-02 23:18:49 +0400

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+import { stdout } from "process";
+const { createHash } = await import("crypto");
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = __dirname + "/files/fileToCalculateHashFor.txt";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const input = createReadStream(filePath);
+  return input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
+
+await calculateHash();
