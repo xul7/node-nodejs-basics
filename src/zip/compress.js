0b2a79409857cc906f7aff5083812ac70579a6a3
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-03 20:13:59 +0400

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,23 @@
-export const compress = async () => {
-    // Write your code here 
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourceFilePath = __dirname + "/files/fileToCompress.txt";
+const destinationFilePath = __dirname + "/files/archive.gz";
+
+export const compress = async (input, output) => {
+  const pipe = promisify(pipeline);
+  const gzip = createGzip();
+  const source = createReadStream(input);
+  const destination = createWriteStream(output);
+  await pipe(source, gzip, destination);
 };
+
+compress(sourceFilePath, destinationFilePath).catch((err) => {
+  console.error("An error occurred:", err);
+  process.exitCode = 1;
+});
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,27 @@
-export const decompress = async () => {
-    // Write your code here 
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { promisify } from "util";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+console.log(
+  "to check correct work 1 - do compress.js, 2 - delete fileToCompress.txt, 3 - do decompress.js"
+);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourceFilePath = __dirname + "/files/archive.gz";
+const destinationFilePath = __dirname + "/files/fileToCompress.txt";
+
+export const decompress = async (input, output) => {
+  const pipe = promisify(pipeline);
+  const gunzip = createGunzip();
+  const source = createReadStream(input);
+  const destination = createWriteStream(output);
+  await pipe(source, gunzip, destination);
 };
+
+decompress(sourceFilePath, destinationFilePath).catch((err) => {
+  console.error("An error occurred:", err);
+  process.exitCode = 1;
+});
