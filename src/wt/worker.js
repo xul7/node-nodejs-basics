a7eb43651077b4571c51d748b406d3860909d381
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-05 16:49:42 +0400

    feat: wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,26 @@
-// n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { Worker, isMainThread, parentPort } from "worker_threads";
+import { fileURLToPath } from "url";
 
-export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const __filename = fileURLToPath(import.meta.url);
+
+// n should be received from main thread
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+export const sendResult = (n) => {
+  // This function sends result of nthFibonacci computations to main thread
+  return new Promise((resolve, reject) => {
+    if (isMainThread) {
+      const worker = new Worker(__filename);
+      worker.postMessage(n);
+      worker.on("message", resolve);
+      worker.on("error", reject);
+    } else {
+      parentPort.on("message", (data) => {
+        parentPort.postMessage(nthFibonacci(data));
+      });
+    }
+  });
 };
+
+console.log("result from worker is", await sendResult(10));
