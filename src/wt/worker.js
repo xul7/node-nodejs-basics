48560fd5fe090207bc93e9ccec1e1dde078eb3f2
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2022-06-05 16:56:24 +0400

    fix: wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,4 +1,4 @@
-import { Worker, isMainThread, parentPort } from "worker_threads";
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,14 +11,18 @@ export const sendResult = (n) => {
   // This function sends result of nthFibonacci computations to main thread
   return new Promise((resolve, reject) => {
     if (isMainThread) {
-      const worker = new Worker(__filename);
-      worker.postMessage(n);
+      const worker = new Worker(__filename, {
+        workerData: n,
+      });
       worker.on("message", resolve);
       worker.on("error", reject);
-    } else {
-      parentPort.on("message", (data) => {
-        parentPort.postMessage(nthFibonacci(data));
+      worker.on("exit", (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
       });
+    } else {
+      const number = workerData;
+      parentPort.postMessage(nthFibonacci(number));
     }
   });
 };
